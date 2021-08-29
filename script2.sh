6daedf15e7839c49b64ffc6ae46e176357cac11e
halod2003 <44163382+halod2003@users.noreply.github.com>
2021-08-29 19:34:22 +1000

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -29,10 +29,45 @@ echo Thank you $IPAddr
 
 echo Installing Docker and Docker compose
 
-sudo curl -sSL https://get.docker.com | sh
-
+sudo apt-get install docker-ce -qq > /dev/null
 sudo usermod -aG docker pi
-sudo pip3 install docker-compose --quiet
 sudo systemctl enable docker
 
-echo Docker Installed
+sudo pip3 install docker-compose --quiet
+
+echo Docker and Docker compose Installed
+
+## 3) Create supporting direcoties and files
+
+##For Pi-Hole
+
+cd /home/pi
+mkdir pihole
+cd pihole
+mkdir pihole
+mkdir dnsmasq.d
+cd /home/pi
+
+#For Prometheus
+mkdir prometheus
+cd prometheus
+curl https://raw.githubusercontent.com/halod2003/pihole/main/prometheus.yml -o prometheus.yml
+
+## 4) Install services using docker-compose
+
+PiExpoInfo='        - PIHOLE_HOSTNAME='
+
+curl https://raw.githubusercontent.com/halod2003/pihole/main/docker-compose.yml -o docker-compose.yml
+echo "$PiExpoInfo$IPAddr" >> docker-compose.yml
+
+##Start containers using docker-compose files
+
+sudo docker-compose -f docker-compose.yml up -d
+
+echo Installation complete
+echo "Access Information"
+echo "------------------"
+echo "Pi-hole http://$IPAddr"
+echo "Grafana http://$IPAddr:3000"
+echo "Portainer http://$IPAddr:9000"
+echo "Prometheus http://$IPAddr:9090"
